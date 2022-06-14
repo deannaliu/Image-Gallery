2b68be689771ba9dc50f1dd856affb3b7ce98cd4
Deanna <deanna.mei.liu@gmail.com>
2022-06-13 22:29:06 -0400

    bind enter to search button

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ input.addEventListener("input", (e)=>{
     query = e.target.value;
 });
 
+ // If the user presses the "Enter" key on the keyboard
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      document.getElementById("main_search").click();
+    }
+  });
+
 async function CuratedPhotos(pagenr){
     const data = await fetch(
         `https://api.pexels.com/v1/curated?per_page=15&page=${pagenr}`, 
@@ -35,6 +45,44 @@ async function CuratedPhotos(pagenr){
     //console.log(result);
 }
 
+async function SearchPhotos(query, pagenr){
+    const data = await fetch(
+        `https://api.pexels.com/v1/search?query=${query}&per_page=15&page=${pagenr}`, 
+        {
+            method:"GET",
+            headers:{
+                Accept: "application/json",
+                Authorization: auth,
+            },
+        }
+    );
+    const result = await data.json();
+    result.photos.forEach(photo => {
+        const pic = document.createElement("div");
+        pic.innerHTML=`<img src=${photo.src.large}>
+            <p>Photo: ${photo.photographer}</p>
+            <a href=${photo.src.large}>Download</a>
+        `;
+        document.querySelector(".gallery").appendChild(pic);
+    });
+    //console.log(result);
+}
+
+searchbutton.addEventListener("click", ()=> {
+    if(input.value==="")return;
+    clear();
+    search = true;
+    SearchPhotos(query, pagenr);
+    pagenr++;
+
+});
+
+function clear(){
+    input.value="";
+    document.querySelector(".gallery").innerHTML="";
+    pagenr = 1;
+}
+
 next.addEventListener("click", ()=>{
     if (!search){ 
         // if we dont have any input, click next button, page will load auto next 15
@@ -42,5 +90,10 @@ next.addEventListener("click", ()=>{
         pagenr++;
         CuratedPhotos(pagenr);
     }
-})
+    else{
+        if (query.value==="") return;
+            pagenr++;
+            SearchPhotos(query, pagenr);
+    }
+});
 CuratedPhotos(pagenr)
